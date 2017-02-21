7bc2503dcde94cfad9f79739baf52faf3f61d649
Brian Bycynski <high5bri@hotmail.com>
2017-02-21 13:55:33 -0500

    still working

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,9 @@
 class ProductList extends React.Component {
   render() {
     return (
-      // <div className='ui unstackable items'>
+       <div className='ui unstackable items'>
       <Product />
-      <Bdog />
-      {/* </div> */}
+      </div>
     );
   }
 }
@@ -31,15 +30,7 @@ class Product extends React.Component {
     );
   }
 }
-class Bdog extends React.component {
-  render() {
-    return (
-      <div>
-      <h1>bdog</h1>
-      </div>
-    );
-  }
-}
+
 
 ReactDOM.render(
   <ProductList />,
